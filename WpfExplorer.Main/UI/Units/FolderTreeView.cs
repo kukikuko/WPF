fc70ecc640d3319a0996343710a27e99030b246e
kukikuko <gurwn805@naver.com>
2025-05-02 11:40:31 +0900

    FolderTreeItem UI, Trigger

--- WpfExplorer.Main/UI/Units/FolderTreeView.cs
+++ WpfExplorer.Main/UI/Units/FolderTreeView.cs
@@ -1,13 +1,14 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using WpfExplorer.Support.Local.Models;
 
 namespace WpfExplorer.Main.UI.Units
 {
     public class FolderTreeView : TreeView
     {
         public static readonly DependencyProperty SelectionCommandProperty =
-            DependencyProperty.Register("SelectionComand", typeof(ICommand), typeof(FolderTreeView));
+            DependencyProperty.Register(nameof(SelectionCommand), typeof(ICommand), typeof(FolderTreeView));
 
         static FolderTreeView()
         {
@@ -24,6 +25,19 @@ namespace WpfExplorer.Main.UI.Units
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new FolderTreeItem();
+        }
+
+        public FolderTreeView()
+        {
+            SelectedItemChanged += TreeView_SelectedItemChanged;
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (SelectedItem is FolderInfo item)
+            {
+                SelectionCommand?.Execute(item);
+            }
         }
     }
 }
--- WpfExplorer.Support/Local/Helpers/FileService.cs
+++ WpfExplorer.Support/Local/Helpers/FileService.cs
@@ -1,4 +1,6 @@
-﻿using Jamesnet.Wpf.Controls;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Jamesnet.Wpf.Controls;
 using System.IO;
 using WpfExplorer.Support.Local.Models;
 
@@ -41,6 +43,50 @@ namespace WpfExplorer.Support.Local.Helpers
                 FullPath = fullPath,
                 Children = new()
             };
+        }
+
+        public void RefreshSubdirectories(FolderInfo parent)
+        {
+            var newChildren = FetchSubdirectories(parent);
+
+            var oldChildrenDict = parent.Children.ToDictionary(c => c.FullPath);
+            var newChildrenDict = newChildren.ToDictionary(c => c.FullPath);
+
+            var added = newChildren.Where(c => !oldChildrenDict.ContainsKey(c.FullPath)).ToList();
+            var removed = parent.Children.Where(c => !newChildrenDict.ContainsKey(c.FullPath)).ToList();
+
+            parent.Children.AddRange(added);
+            foreach (var child in removed)
+            {
+                parent.Children.Remove(child);
+            }
+        }
+
+        private static List<FolderInfo> FetchSubdirectories(FolderInfo parent)
+        {
+            var children = new List<FolderInfo>();
+
+            try
+            {
+                var subDirs = Directory.GetDirectories(parent.FullPath);
+                foreach (var dir in subDirs)
+                {
+                    children.Add(new FolderInfo
+                    {
+                        Depth = parent.Depth + 1,
+                        Name = Path.GetFileName(dir),
+                        IconType = IconType.Folder,
+                        FullPath = dir,
+                        Children = new()
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return children;
         }
     }
 }
