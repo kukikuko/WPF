2bd51def21042307c3cdf2910a6ed52b20326ff6
kukikuko <gurwn805@naver.com>
2025-05-13 09:34:39 +0900

    LocationInfo 파일 생성

---
+++ WpfExplorer.Support/Local/Models/LocationInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfExplorer.Support.Local.Models
+{
+    public class LocationInfo : FileInfoBase
+    {
+        public int Zindex { get; set; }
+        public string Color { get; set; }
+        public bool IsRoot { get; set; }
+        public bool IsLast { get; set; }
+
+        public LocationInfo(string name, string fullName)
+        {
+            Name = name;
+            FullPath = fullName;
+        }
+    }
+}
