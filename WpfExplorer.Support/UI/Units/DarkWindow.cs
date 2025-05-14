29a6c55865b5b3785cf04ae22ea014f1c2351c7c
kukikuko <gurwn805@naver.com>
2025-05-14 17:37:57 +0900

    DarkWindow 고도화

--- WpfExplorer.Support/UI/Units/DarkWindow.cs
+++ WpfExplorer.Support/UI/Units/DarkWindow.cs
@@ -5,6 +5,26 @@ namespace WpfExplorer.Support.UI.Units
 {
     public class DarkWindow : JamesWindow
     {
+        public static readonly DependencyProperty LocationTemplateProperty = 
+            DependencyProperty.Register("LocationTemplate", typeof(DataTemplate),
+                typeof(DarkWindow), new UIPropertyMetadata(null));
+
+        public DataTemplate LocationTemplate
+        {
+            get { return (DataTemplate)GetValue(LocationTemplateProperty); }
+            set { SetValue(LocationTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty LocationProperty = 
+            DependencyProperty.Register("Location", typeof(object),
+                typeof(DarkWindow), new UIPropertyMetadata(null));
+
+        public object Location
+        {
+            get { return (object)GetValue(LocationTemplateProperty); }
+            set { SetValue(LocationTemplateProperty, value); }
+        }
+
         static DarkWindow()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(DarkWindow), 
