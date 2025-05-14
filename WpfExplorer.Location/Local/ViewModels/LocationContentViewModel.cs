6ecfbbd4d5858e9609ca9ec91060e16c179ecfa1
kukikuko <gurwn805@naver.com>
2025-05-14 17:18:32 +0900

    LocatorTextBox 현재 위치 연결

--- WpfExplorer.Location/Local/ViewModels/LocationContentViewModel.cs
+++ WpfExplorer.Location/Local/ViewModels/LocationContentViewModel.cs
@@ -1,6 +1,8 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Jamesnet.Wpf.Mvvm;
 using WpfExplorer.Support.Local.Helpers;
+using WpfExplorer.Support.Local.Models;
 
 namespace WpfExplorer.Location.Local.ViewModels
 {
@@ -8,9 +10,17 @@ namespace WpfExplorer.Location.Local.ViewModels
     {
         private readonly NavigatorService _navigatorService;
 
+        [ObservableProperty] private FileInfoBase _current;
+
         public LocationContentViewModel(NavigatorService navigatorService)
         {
             _navigatorService = navigatorService;
+            _navigatorService.LocationChanged += _navigatorService_LocationChanged;
+        }
+
+        private void _navigatorService_LocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            Current = e.Current;
         }
 
         [RelayCommand]
